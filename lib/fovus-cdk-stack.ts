a2cfe4aa4ba7c4f59c3ceb0f6b09a3f610d92499
shrey333 <shreybahdiyadara33@gmail.com>
2024-04-29 14:05:26 -0700

    Feat: init project and create dynamodb table

--- lib/fovus-cdk-stack.ts
+++ lib/fovus-cdk-stack.ts
@@ -1,16 +1,21 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { AttributeType, StreamViewType, Table } from "aws-cdk-lib/aws-dynamodb";
+import { Construct } from "constructs";
 
 export class FovusCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    this.createDynamoDBTable();
+  }
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'FovusCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+  private createDynamoDBTable(): Table {
+    return new Table(this, this.stackName + "DynamoDB", {
+      partitionKey: {
+        name: "id",
+        type: AttributeType.STRING,
+      },
+      tableName: this.stackName + "DynamoDB",
+      stream: StreamViewType.NEW_IMAGE,
+    });
   }
 }
