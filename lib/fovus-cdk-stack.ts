1dd07c0e41203ce719c564448a9864f691a54ba6
shrey333 <shreybahdiyadara33@gmail.com>
2024-04-29 21:51:46 -0700

    Feat: add step function

--- lib/fovus-cdk-stack.ts
+++ lib/fovus-cdk-stack.ts
@@ -5,7 +5,14 @@ import {
   RestApi,
 } from "aws-cdk-lib/aws-apigateway";
 import { AttributeType, StreamViewType, Table } from "aws-cdk-lib/aws-dynamodb";
-import { Role, ServicePrincipal } from "aws-cdk-lib/aws-iam";
+import {
+  CfnInstanceProfile,
+  Effect,
+  PolicyDocument,
+  PolicyStatement,
+  Role,
+  ServicePrincipal,
+} from "aws-cdk-lib/aws-iam";
 import {
   ApplicationLogLevel,
   LoggingFormat,
@@ -26,6 +33,7 @@ import {
   HttpMethods,
 } from "aws-cdk-lib/aws-s3";
 import { BucketDeployment, Source } from "aws-cdk-lib/aws-s3-deployment";
+import { DefinitionBody, StateMachine } from "aws-cdk-lib/aws-stepfunctions";
 import { Construct } from "constructs";
 import { join } from "path";
 
@@ -33,9 +41,13 @@ export class FovusCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
     const dynamoTable = this.createDynamoDBTable();
+
     const bucket = this.createS3Bucket();
     this.uploadScriptToS3(bucket);
+
     this.createApiGateway(dynamoTable);
+
+    this.createStepFunction(dynamoTable, bucket);
   }
 
   private createDynamoDBTable(): Table {
@@ -138,5 +150,77 @@ export class FovusCdkStack extends cdk.Stack {
     );
 
     return restApi;
+  }
+
+  private createStepFunction(dynamoTable: Table, bucket: Bucket): StateMachine {
+    const ec2Role = new Role(this, this.stackName + "EC2Role", {
+      roleName: this.stackName + "EC2Role",
+      assumedBy: new ServicePrincipal("ec2.amazonaws.com"),
+    });
+
+    dynamoTable.grantReadWriteData(ec2Role);
+    bucket.grantReadWrite(ec2Role);
+
+    const instanceProfile = new CfnInstanceProfile(
+      this,
+      this.stackName + "InstanceProfile",
+      {
+        instanceProfileName: this.stackName + "InstanceProfile",
+        roles: [ec2Role.roleName],
+      }
+    );
+
+    const stepFunctions = {
+      StartAt: "StartEC2Instance",
+      States: {
+        StartEC2Instance: {
+          Type: "Task",
+          Resource: "arn:aws:states:::aws-sdk:ec2:runInstances",
+          Parameters: {
+            ImageId: "ami-080e1f13689e07408",
+            InstanceType: "t2.micro",
+            MinCount: 1,
+            MaxCount: 1,
+            IamInstanceProfile: { Arn: instanceProfile.attrArn },
+            Monitoring: { Enabled: true },
+            InstanceInitiatedShutdownBehavior: "terminate",
+            "UserData.$": `States.Base64Encode(States.Format('#!/bin/bash\nsudo apt-get update -y\nsudo apt install python3-pip unzip -y\npip install boto3\ncurl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"\nunzip awscliv2.zip\nsudo ./aws/install\naws s3 cp s3://fovus-stack-s3-bucket/python_script.py python_script.py\npython3 python_script.py {} ${dynamoTable.tableName}\nshutdown -h now', $.id))`,
+          },
+          End: true,
+          InputPath: "$[0]",
+        },
+      },
+    };
+
+    const stateMachineRole = new Role(
+      this,
+      this.stackName + "StateMachineRole",
+      {
+        roleName: this.stackName + "StateMachineRole",
+        assumedBy: new ServicePrincipal("states.amazonaws.com"),
+        inlinePolicies: {
+          EC2Policy: new PolicyDocument({
+            statements: [
+              new PolicyStatement({
+                effect: Effect.ALLOW,
+                actions: [
+                  "ec2:RunInstances",
+                  "ec2:CreateTags",
+                  "iam:PassRole",
+                  "ssm:GetParameters",
+                ],
+                resources: ["*"],
+              }),
+            ],
+          }),
+        },
+      }
+    );
+
+    return new StateMachine(this, this.stackName + "StateMachine", {
+      stateMachineName: this.stackName + "StateMachine",
+      definitionBody: DefinitionBody.fromString(JSON.stringify(stepFunctions)),
+      role: stateMachineRole,
+    });
   }
 }
